47285a92ca61cc35dabc20a51cb8a1f512006714
iceboy95 <hqkhor95@EMAIL.com>
2016-02-02 22:18:19 +0800

    no idea, thinking

---
+++ q3.rb
@@ -0,0 +1,10 @@
+puts  "==============================="
+puts  "======Combination Script======="
+puts  "==============================="
+
+puts "Press 1 for text file input"
+puts "Press 2 for string input"
+
+
+#calling rf.rb script directly
+system("ruby","rf.rb");
