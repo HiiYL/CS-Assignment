220b508e8380896906f017abe42982304b80e015
iceboy95 <hqkhor95@EMAIL.com>
2016-02-03 15:54:11 +0800

    some interface changes in q3

--- cipher.rb
+++ cipher.rb
@@ -14,6 +14,7 @@ class Caesar
   def decrypt(string)
     string.downcase.tr(@encrypt, @decrypt)
   end
+  
 end
 
 puts "*** Caesar Cipher ***"
--- q3.rb
+++ q3.rb
@@ -1,3 +1,4 @@
+
 puts  "==============================="
 puts  "======Combination Script======="
 puts  "==============================="
@@ -5,6 +6,25 @@ puts  "==============================="
 puts "Press 1 for text file input"
 puts "Press 2 for string input"
 
+decision = gets.chomp.to_i
+if decision == 1
 
+elsif decision == 2
+	puts "Please input your plaintext"
+	plaintext = gets.chomp
+end
+
+puts "Press 1 for Caesar Cipher Encryption"
+puts "Press 2 for Rail Fence Cipher Encryption"
+decisionToEncrypt = gets.chomp.to_i
+if decisionToEncrypt == 1
+	puts "Please input the key: "
+	key = gets.chomp
+	text = Caesar.new(key)
+	puts text.encrypt(plaintext)
+elsif decisionToEncrypt == 2
+
+end 
+	
 #calling rf.rb script directly
-system("ruby","rf.rb");
+#system("ruby","rf.rb");
